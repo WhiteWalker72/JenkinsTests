737cca6886cf703a3c09c486c319966abaedc5c8
Sivar72 <sivarwerrie@hotmail.nl>
2018-02-21 12:26:08 +0100

    Initial Commit

---
+++ src/main/java/net/hu/jenkins/Program.java
@@ -0,0 +1,9 @@
+package net.hu.jenkins;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/net/hu/jenkins/SalaryCalculator.java
@@ -0,0 +1,30 @@
+package net.hu.jenkins;
+
+public class SalaryCalculator {
+
+    private double salary;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary < 0) {
+            throw new IllegalArgumentException("Salary can't be negative.");
+        }
+        this.salary = salary;
+    }
+
+    public double getInsuranceMoney() {
+        return (salary * 15) / 100;
+    }
+
+    public double getSalaryBonus() {
+        return salary * 0.1;
+    }
+
+    public double getTotalSalary() {
+        return getSalary() + getInsuranceMoney() + getSalaryBonus();
+    }
+
+}
